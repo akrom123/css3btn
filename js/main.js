0fd8e9ff965548c58869d70ae9a3393e2d3d8aa0
Saidakrom <akirom123@ya.ru>
2014-08-03 15:25:23 +0500

    Работающие слайдеры

--- js/main.js
+++ js/main.js
@@ -3,8 +3,36 @@
 	var app = {
 		init: function () {
 			$( ".slider" ).slider({ animate: "fast" });
+            $('.ui-radius').slider({
+                min: 0,
+                max: 50,
+                slide: function(event, ui) {
+                    $('.changed').html(ui.value);
+                }
+            });
+            $('.ui-border_size').slider({
+                min: 0,
+                max: 50,
+                slide: function(event, ui) {
+                    $('.changed').html(ui.value);
+                }
+            });
+            $('.ui-padding_x').slider({
+                min: 0,
+                max: 50,
+                slide: function(event, ui) {
+                    $('.changed').html(ui.value);
+                }
+            });
+            $('.ui-padding_y').slider({
+                min: 0,
+                max: 50,
+                slide: function(event, ui) {
+                    $('.changed').html(ui.value);
+                }
+            });
 		}
-	}
+	};
 
 	app.init();
 
